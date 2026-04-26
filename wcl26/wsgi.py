c680472bfe5778ff926baa1d01bba418ada3b06d
ansarikhursaid7tr <er.khursaid7tr@gmail.com>
2026-04-26 14:48:29 -0500

    Auto migrate on WSGI init for Vercel deployment

--- wcl26/wsgi.py
+++ wcl26/wsgi.py
@@ -13,3 +13,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wcl26.settings')
 
 application = get_wsgi_application()
+
+# Run database migrations automatically when Vercel spins up the WSGI container
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print("Migration failed during WSGI init:", str(e))
