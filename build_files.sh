48de7c3e861476ee90fafbff589e0d83274ca64f
ansarikhursaid7tr <er.khursaid7tr@gmail.com>
2026-04-26 14:45:16 -0500

    Run database migrations during Vercel build

--- build_files.sh
+++ build_files.sh
@@ -7,4 +7,7 @@ python3 -m pip install -r requirements.txt --break-system-packages || pip instal
 echo ">>> Collecting static files..."
 python3 manage.py collectstatic --noinput || python manage.py collectstatic --noinput
 
+echo ">>> Running Database Migrations..."
+python3 manage.py migrate --noinput || python manage.py migrate --noinput
+
 echo ">>> Build complete!"
