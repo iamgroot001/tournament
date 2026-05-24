4c8e5e64adab46532ea10d7536ee8f1e8b4d4c43
ansarikhursaid7tr <er.khursaid7tr@gmail.com>
2026-05-23 19:09:59 -0500

    fix: auto-run migrations on wsgi load

--- wcl26/wsgi.py
+++ wcl26/wsgi.py
@@ -14,3 +14,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wcl26.settings')
 
 application = get_wsgi_application()
 
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Failed to run automatic migrations: {e}")
